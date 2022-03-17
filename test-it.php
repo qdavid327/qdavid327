dc1322a2f17dfe7b6cdf9cda225f721bb90485f3
qlara <qdavid327@gmail.com>
2022-03-17 17:41:41 -0400

    Guarda lo que te indico.

---
+++ test-it.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba con Git</title>
+</head>
+<body>
+    Esto es una prueba con <strong>Git</strong>
+    <br>
+    <?php "Potencia de 2 a la 10: " . pow(2,10); ?>
+</body>
+</html>l
